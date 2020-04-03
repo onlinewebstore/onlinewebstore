7ee4572470ae1586d02b9f33949ea2e3f04ae999
youssefadel <youssefadel92@gmail.com>
2020-04-03 16:19:22 +0200

    Update User.php
    made it abstract class

--- app/User.php
+++ app/User.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
 
-class User extends Authenticatable
+abstract class User extends Authenticatable
 {
     use Notifiable;
 
