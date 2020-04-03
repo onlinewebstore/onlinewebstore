6a939b12d844b51eea9fe44870923668472a239e
youssefadel <youssefadel92@gmail.com>
2020-04-03 16:15:29 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('registerbuyer', 'RegisterController@createBuyer'); 
+Route::get('registeradmin', 'RegisterController@createAdmin')->middleware('verifyadmin');
+Route::get('registerowner', 'RegisterController@createOwner'); 
+//Route::get('loginbuyer', 'LoginController@buyerLogin')->middleware('guest:buyer')->middleware('guest:admin')->middleware('guest:owner');
+//Route::get('loginadmin', 'LoginController@adminLogin')->middleware('guest:buyer')->middleware('guest:admin')->middleware('guest:owner');
+//Route::get('loginowner', 'LoginController@ownerLogin')->middleware('guest:buyer')->middleware('guest:admin')->middleware('guest:owner');
+Route::get('logout','LoginController@logout');
+Route::get('login','LoginController@login')->middleware('guest:buyer')->middleware('guest:admin')->middleware('guest:owner');
+Route::get('getallusers','UserController@getAllusers')->middleware('verifyadmin');
+Route::get('test','LoginController@test');
